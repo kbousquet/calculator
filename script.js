d1ec59239ce6120074f6550a5c30ff513796bc62
Kathryn Bousquet <72675497+kbousquet@users.noreply.github.com>
2022-04-02 20:34:32 -0500

    make edits

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-function add(num1, num2) {
-	return num1 + num2;
-};
+// Call buttons
+const sevenButton = document.getElementById('seven-button');
+const eightButton = document.getElementById('eight-button');
+const nineButton = document.getElementById('nine-button');
+const divideButton = document.getElementById('divide-button');
+const fourButton = document.getElementById('four-button');
+const fiveButton = document.getElementById('five-button');
+const sixButton = document.getElementById('six-button');
+const multiplyButton = document.getElementById('multiply-button');
+const oneButton = document.getElementById('one-button');
+const twoButton = document.getElementById('two-button');
+const threeButton = document.getElementById('three-button');
+const minusButton = document.getElementById('minus-button');
+const clearButton = document.getElementById('clear-button');
+const zeroButton = document.getElementById('zero-button');
+const equalButton = document.getElementById('equal-button');
+const plusButton = document.getElementById('plus-button');
 
-function subtract(num1, num2) {
-	return num1 - num2;
-};
+// Call display values
+let valueInput = document.getElementById('value-input');
+let calcText = document.getElementById('calculation');
+let calcValue = 0;
 
-function multiply(num1, num2) {
-    return num1 * num2
-};
 
-function divide(num1, num2) {
-    return num1 / num2
-  };
 
 function operate(num1, operator, num2){
+    
     switch(operator){
         case '+':
-            return(add(num1,num2));
+            calcValue = num1 + num2;
+            calcText.innerText = `${calcValue}`;
+            break;
         case '-':
-            return(subtract(num1,num2));
+            calcValue = num1 - num2;
+            calcText.innerText = `${calcValue}`;
+            break;
         case '*':
-            return(multiply(num1,num2));
+            calcValue = num1 * num2;    
+            calcText.innerText = `${calcValue}`;
+            break;
         case '/':
-            return(divide(num1,num2));
+            calcValue = num1 / num2;
+            calcText.innerText = `${calcValue}`;
+            break;
     }
+
 }
+
+function populateDisplay(num1, operator, num2){
+    valueInput.innerText = `${num1} ${operator} ${num2}`;
+}
+
+
+populateDisplay(2,'*',5);
+
+operate(2,'*',5);
