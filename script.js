8e84784357704339c67da23644f16d4138d1bd84
Kathryn Bousquet <72675497+kbousquet@users.noreply.github.com>
2022-04-03 22:55:19 -0500

    add edits

--- script.js
+++ script.js
@@ -1,56 +1,94 @@
 // Call buttons
-const sevenButton = document.getElementById('seven-button');
-const eightButton = document.getElementById('eight-button');
-const nineButton = document.getElementById('nine-button');
-const divideButton = document.getElementById('divide-button');
-const fourButton = document.getElementById('four-button');
-const fiveButton = document.getElementById('five-button');
-const sixButton = document.getElementById('six-button');
-const multiplyButton = document.getElementById('multiply-button');
-const oneButton = document.getElementById('one-button');
-const twoButton = document.getElementById('two-button');
-const threeButton = document.getElementById('three-button');
-const minusButton = document.getElementById('minus-button');
-const clearButton = document.getElementById('clear-button');
-const zeroButton = document.getElementById('zero-button');
-const equalButton = document.getElementById('equal-button');
-const plusButton = document.getElementById('plus-button');
+const equalButton = document.querySelector('.equal-button');
+const operandButtons = document.querySelectorAll('.operation-button');
+const digitButtons = document.querySelectorAll('.digit-button');
 
-// Call display values
-let valueInput = document.getElementById('value-input');
-let calcText = document.getElementById('calculation');
-let calcValue = 0;
+// Call display elements
+const valueInput = document.getElementById('value-input');
+const ansText = document.getElementById('calculation');
 
+// Store num1, num2, and operator inputs
+let num1 = [];
+let num1Display = '';
+let num1Input;  // store num1 as an integer for the operate function
+
+let num2 = [];
+let num2Display = '';
+let num2Input;  // store num2 as an integer for the operate function
+
+let operatorInput;
+
+// Store answer variable
+let ans;
+
+// Trigger handlers
+let operandClicked = false;
+let calcContinue = false;
+
+operandButtons.forEach(button => button.addEventListener('click',()=>{
+    valueInput.textContent = button.innerText;
+    operandClicked = true;
+    operatorInput = button.innerText;
+    num1Input = parseInt(num1Display);
+    runCalculator()
+}));
+
+// Handle equal button
+equalButton.addEventListener('click', () => {
+    equalClicked = true;
+    num2Input = parseInt(num1Display);
+    operate(num1Input, operatorInput, num2Input);
+    num1Input = ans;
+    calcContinue = true;
+    runCalculator();
+});
+
+function runCalculator(){
+    
+    // Parse buttons to num2 after the operand has been clicked
+    if (operandClicked && !calcContinue){
+        digitButtons.forEach(button => button.addEventListener('click',()=>{
+            num2.push(button.innerText);
+            num2Display = +num2.join("");
+            valueInput.textContent = num2Display;
+            valueInput.style.visibility = 'visible';
+        }));
+    } 
+
+    // Parse buttons to num1 before the operand has been clicked
+    else if (!operandClicked && !calcContinue){    
+        digitButtons.forEach(button => button.addEventListener('click',()=>{
+            num1.push(button.innerText);
+            num1Display = +num1.join("");
+            valueInput.textContent = num1Display;
+            valueInput.style.visibility = 'visible';
+        }));
+    }
+    
+};
 
 
 function operate(num1, operator, num2){
-    
     switch(operator){
         case '+':
-            calcValue = num1 + num2;
-            calcText.innerText = `${calcValue}`;
+            ans = num1 + num2;
+            ansText.innerText = `${ans}`;
             break;
         case '-':
-            calcValue = num1 - num2;
-            calcText.innerText = `${calcValue}`;
+            ans = num1 - num2;
+            ansText.innerText = `${ans}`;
             break;
-        case '*':
-            calcValue = num1 * num2;    
-            calcText.innerText = `${calcValue}`;
+        case 'x':
+            ans = num1 * num2;    
+            ansText.innerText = `${ans}`;
             break;
-        case '/':
-            calcValue = num1 / num2;
-            calcText.innerText = `${calcValue}`;
+        case '÷':
+            ans = num1 / num2;
+            ansText.innerText = `${ans}`;
             break;
     }
 
 }
 
-function populateDisplay(num1, operator, num2){
-    valueInput.innerText = `${num1} ${operator} ${num2}`;
-}
 
-
-populateDisplay(2,'*',5);
-
-operate(2,'*',5);
+runCalculator();
