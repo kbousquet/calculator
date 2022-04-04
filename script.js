1f6e44186fbbd3f14f2a06cae57c6195c929503d
Kathryn Bousquet <72675497+kbousquet@users.noreply.github.com>
2022-04-04 13:24:13 -0500

    edit basic calculator logic

--- script.js
+++ script.js
@@ -25,69 +25,108 @@ let ans;
 let operandClicked = false;
 let calcContinue = false;
 
-operandButtons.forEach(button => button.addEventListener('click',()=>{
-    valueInput.textContent = button.innerText;
-    operandClicked = true;
-    operatorInput = button.innerText;
-    num1Input = parseInt(num1Display);
-    runCalculator()
-}));
 
-// Handle equal button
-equalButton.addEventListener('click', () => {
-    equalClicked = true;
-    num2Input = parseInt(num1Display);
-    operate(num1Input, operatorInput, num2Input);
-    num1Input = ans;
-    calcContinue = true;
-    runCalculator();
-});
+
+function resetInputs(){
+    num1 = [];
+    num1Display = '';
+    num1Input = ans;  
+    ans = 0;
+
+    num2 = [];
+    num2Display = '';
+    num2Input = 0;
+
+    operatorInput = '';
+}
 
 function runCalculator(){
-    
-    // Parse buttons to num2 after the operand has been clicked
-    if (operandClicked && !calcContinue){
-        digitButtons.forEach(button => button.addEventListener('click',()=>{
-            num2.push(button.innerText);
-            num2Display = +num2.join("");
-            valueInput.textContent = num2Display;
-            valueInput.style.visibility = 'visible';
-        }));
-    } 
-
-    // Parse buttons to num1 before the operand has been clicked
-    else if (!operandClicked && !calcContinue){    
-        digitButtons.forEach(button => button.addEventListener('click',()=>{
+    // Digit button triggers
+    digitButtons.forEach(button => button.addEventListener('click',()=>{
+        // First run - Parse buttons to num1 before the operand has been clicked
+        if (!operandClicked){
             num1.push(button.innerText);
             num1Display = +num1.join("");
             valueInput.textContent = num1Display;
             valueInput.style.visibility = 'visible';
-        }));
-    }
-    
-};
+        } 
+        // Parse buttons to num2 after the operand has been clicked
+        else {
+            num2.push(button.innerText);
+            num2Display = +num2.join("");
+            valueInput.textContent = num2Display;
+        }
+    }));
+
+    // Operand buttons
+    operandButtons.forEach(button => button.addEventListener('click',()=>{
+        // Throw error if user clicks an operand before inputting num1
+        if (!operandClicked && num1.length === 0){
+            valueInput.style.visibility = 'visible';
+            valueInput.textContent = 'ERROR';
+        }
+        // Operator on first run
+        else if (!operandClicked){
+            valueInput.textContent = button.innerText;
+            operandClicked = true;
+            operatorInput = button.innerText;
+            num1Input = parseInt(num1Display);
+        }
+        // Operator on subsequent runs
+        else {
+            valueInput.textContent = button.innerText;
+            operatorInput = button.innerText;
+        }
+    }));
+
+    // Equal button
+    equalButton.addEventListener('click', () => {
+        // Throw error if user tries to divide by 0
+        if (operatorInput === '÷' && num2Display === 0){
+            valueInput.textContent = 'ERROR';
+            calcContinue = false;
+            resetInputs();
+            num1Input=0;
+        }
+        // Handle equal button on first run
+        else if (!calcContinue){
+            num2Input = parseInt(num2Display);
+            operate(num1Input, operatorInput, num2Input);
+            calcContinue = true;
+            resetInputs()
+        }
+        else {
+            num2Input = parseInt(num2Display);
+            operate(num1Input, operatorInput, num2Input);
+            resetInputs()
+        }
+    });
+} 
 
 
-function operate(num1, operator, num2){
+    function operate(num1, operator, num2){
     switch(operator){
         case '+':
             ans = num1 + num2;
-            ansText.innerText = `${ans}`;
+            ans = Math.round(ans*100)/100;
+            ansText.innerText = `= ${ans}`;
             break;
         case '-':
             ans = num1 - num2;
-            ansText.innerText = `${ans}`;
+            ans = Math.round(ans*100)/100;
+            ansText.innerText = `= ${ans}`;
             break;
         case 'x':
-            ans = num1 * num2;    
-            ansText.innerText = `${ans}`;
+            ans = num1 * num2;
+            ans = Math.round(ans*100)/100;
+            ansText.innerText = `= ${ans}`;
             break;
         case '÷':
             ans = num1 / num2;
-            ansText.innerText = `${ans}`;
+            ans = Math.round(ans*100)/100;
+            ansText.innerText = `= ${ans}`;
             break;
     }
-
 }
 
 
